d0dbc227d7e0613ca50133016e28163f63f2f750
saraesabbagh <sarasabbagh@hotmail.co.uk>
2023-11-15 15:04:23 +0000

    changed server so that it was truly random page selection rather than just going through different pages each time

--- server/server.js
+++ server/server.js
@@ -7,17 +7,14 @@ const port = 5001;
 app.use(cors());
 app.use(express.json());
 
-let pageCounter = 1;
-
 app.get("/api/disney-characters", async (req, res) => {
   try {
+    const randomPage = getRandomPage();
     const response = await axios.get(
-      `https://api.disneyapi.dev/character?page=${pageCounter}`
+      `https://api.disneyapi.dev/character?page=${randomPage}`
     );
     res.json(response.data);
     console.log(response.data);
-
-    pageCounter++;
   } catch (error) {
     console.error("Error fetching Disney characters:", error);
     res.status(500).send("Internal Server Error");
@@ -26,16 +23,15 @@ app.get("/api/disney-characters", async (req, res) => {
 
 app.post("/api/start-round", async (req, res) => {
   try {
+    const randomPage = getRandomPage();
     const response = await axios.get(
-      `https://api.disneyapi.dev/character?page=${pageCounter}`
+      `https://api.disneyapi.dev/character?page=${randomPage}`
     );
     const characters = response.data.data;
     const selectedCharacter = getRandomCharacter(characters);
 
     res.json({ characters, selectedCharacter });
     console.log(characters);
-
-    pageCounter++;
   } catch (error) {
     console.error("Error starting a new round:", error);
     res.status(500).send("Internal Server Error");
@@ -49,6 +45,11 @@ app.post("/api/submit-answer", (req, res) => {
 
   res.json({ score });
 });
+
+function getRandomPage() {
+  //
+  return Math.floor(Math.random() * 149) + 1;
+}
 
 function getRandomCharacter(characters) {
   const randomIndex = Math.floor(Math.random() * characters.length);
